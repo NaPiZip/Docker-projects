a5510d46eed35736cf83378a1c3c9ba17775d9e1
NaPiZip <napizip@mail.com>
2019-09-09 21:15:03 +0200

    Added setup script for bridge.

---
+++ raspberry_wifi_bridge/configure_bridge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Configuring wifi ethernet bridge
+
+sudo systemctl stop hostapd
+
+# Make safety copy if not already done
+if [ ! -f /etc/dhcpcd.conf.old ]; then
+  sudo mv /etc/dhcpcd.conf /etc/dhcpcd.conf.old
+fi
+
+# Append at the begining
+sudo sed -i '1idenyinterfaces wlan0\ndenyinterfaces eth0' /etc/dhcpcd.conf
+
+sudo brctl addbr br0
+sudo brctl addif br0 eth0
+
+sudo cp bridge-br0.netdev /etc/systemd/network/bridge-br0.netdev
+sudo cp bridge-br0-slave.network /etc/systemd/network/bridge-br0-slave.network
+sudo cp bridge-br0.network /etc/systemd/network/bridge-br0.network
+
+sudo systemctl restart systemd-networkd
+
+# Change accespoit settings
+sudo cp hostapd_bridge.conf /etc/hostapd/hostapd.conf
